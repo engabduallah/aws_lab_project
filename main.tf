4e148407a10542d3e7144a79aa87ff19ff949f50
Abduallah Damash <abduallah.damash@tigahealth.com>
2023-07-29 19:08:46 +0300

    modify some codes

--- main.tf
+++ main.tf
@@ -98,41 +98,41 @@ resource "aws_autoscaling_group" "example" {
 
 }
 
-#module "eks" {
-#  source  = "terraform-aws-modules/eks/aws"
-#  version = "19.15.3"
-#
-#  cluster_name    = local.eks_cluster_name.name
-#  cluster_version = var.eks_cluster_version
-#
-#  vpc_id                         = aws_vpc.main.id
-#  subnet_ids                     = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id, aws_subnet.public_subnet_3.id]
-#  cluster_endpoint_public_access = true
-#
-#  eks_managed_node_group_defaults = {
-#    ami_type = "AL2_x86_64"
-#
-#  }
-#
-#  eks_managed_node_groups = {
-#    one = {
-#      name = local.eks_managed_node_group_first.name
-#
-#      instance_types = var.eks_node_instance_type
-#
-#      min_size     = 1
-#      max_size     = 3
-#      desired_size = 2
-#    }
-#
-#    two = {
-#      name = local.eks_managed_node_group_second.name
-#
-#      instance_types = var.eks_node_instance_type
-#
-#      min_size     = 1
-#      max_size     = 2
-#      desired_size = 1
-#    }
-#  }
-#}
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.3"
+
+  cluster_name    = local.eks_cluster_name.name
+  cluster_version = var.eks_cluster_version
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = local.eks_managed_node_group_first.name
+
+      instance_types = var.eks_node_instance_type
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    two = {
+      name = local.eks_managed_node_group_second.name
+
+      instance_types = var.eks_node_instance_type
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+}
--- versions.tf
+++ versions.tf
@@ -6,6 +6,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.7.0"
     }
-
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.2"
+    }
   }
 }
